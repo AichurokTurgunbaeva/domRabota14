fdd9f5bf4a9dabb74c5fb47c3feafe63cda355cd
Notnik_KG <tasnim0800@gmail.com>
2022-02-05 14:04:38 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Class class1 = new Class(28,"Medina",new int[]{45,12,89,366});
+        System.out.println(class1.getNomer()+" "+class1.getSoz()+" "+ Arrays.toString(class1.getMassiv()));
+
+
+    }
+}
